e6390490ac753fa5e9b56323c09bf8575248f029
batface <zaidanazelart@gmail.com>
2026-04-16 19:16:27 +0700

    add test create mentee project

---
+++ pages/admin/creatementeeregular.ts
@@ -0,0 +1,74 @@
+import { Page, Locator } from '@playwright/test';
+
+type User = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    password: string;
+};
+
+export class CreateMenteeRegular {
+    page: Page;
+
+    menuUserManagement: Locator;
+    addUserBtn: Locator;
+    firstNameInput: Locator;
+    lastNameInput: Locator;
+    emailInput: Locator;
+    phoneInput: Locator;
+
+    roleDropdown: Locator;
+    menteeOption: Locator;
+
+    passwordInput: Locator;
+
+    createUserBtn: Locator;
+    successMessage: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+
+        this.menuUserManagement = page
+            .getByRole('navigation')
+            .getByRole('link', { name: 'User Management' });
+        this.addUserBtn = page.getByRole('button', { name: 'Add User' });
+        this.firstNameInput = page.getByRole('textbox', { name: 'First Name' });
+        this.lastNameInput = page.getByRole('textbox', { name: 'Last Name' });
+        this.emailInput = page.getByRole('textbox', { name: 'Email' });
+        this.phoneInput = page.getByRole('textbox', { name: 'Phone' });
+        this.roleDropdown = page.getByRole('combobox');
+        this.menteeOption = page.getByRole('option', { name: 'Mentee' });
+        this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+        this.createUserBtn = page.getByRole('button', { name: 'Create User' });
+        this.successMessage = page.getByText('User created successfully');
+    }
+
+    getSuccessNotification() {
+        return this.page
+            .getByRole('region', { name: /Notifications/i })
+            .getByText('User created successfully');
+    }
+
+    async goToUserManagement() {
+        await this.menuUserManagement.click();
+    }
+
+    async openCreateUser() {
+        await this.addUserBtn.click();
+    }
+
+    async fillForm(user: User) {
+        await this.firstNameInput.fill(user.firstName);
+        await this.lastNameInput.fill(user.lastName);
+        await this.emailInput.fill(user.email);
+        await this.phoneInput.fill(user.phone);
+        await this.roleDropdown.click();
+        await this.menteeOption.click();
+        await this.passwordInput.fill(user.password);
+    }
+
+    async submit() {
+        await this.createUserBtn.click();
+    }
+}
