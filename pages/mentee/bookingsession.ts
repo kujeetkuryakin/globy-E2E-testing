a7b4c11000bbd3572c4cd50c7a2fecaaa6e438a5
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2026-04-18 16:52:44 +0700

    feat: add pages for booking session

---
+++ pages/mentee/bookingsession.ts
@@ -0,0 +1,94 @@
+import { Locator, Page, expect } from '@playwright/test';
+
+export type CoachForm = {
+  planning: string;
+  description: string;
+  friendNamed: string;
+  date: string;
+  time: string;
+};
+
+enum Method {
+  COACH = 'By CoachChoose your preferred',
+  TOPIC = 'By TopicChoose learning topic',
+  TIME = 'By Date & TimeChoose schedule',
+}
+
+export class BookingSession {
+  page: Page;
+
+  planning: Locator;
+  description: Locator;
+  friendNamed: Locator;
+  mode: Locator[];
+
+  bookSessionBtn: Locator;
+  selectCoachBtn: Locator;
+  confirmAndBookBtn: Locator;
+  confirmBtn: Locator;
+  continueCoachBtn: Locator;
+
+  notificationSuccess: Locator;
+  constructor(page: Page) {
+    this.page = page;
+
+    this.planning = page.getByRole('textbox', { name: 'Contoh: Career Planning,' });
+    this.description = page.getByRole('textbox', { name: 'Add a short description about' });
+    this.friendNamed = page.getByRole('textbox', { name: 'Masukkan nama teman Anda (' });
+    this.mode = [
+      page.getByText('Online SessionVideo call'),
+      page.getByText('Offline SessionFace-to-face'),
+    ];
+
+    this.selectCoachBtn = page.getByRole('button', { name: 'Select' });
+    this.confirmAndBookBtn = page.getByRole('button', { name: 'Confirm & Book Now' });
+    this.bookSessionBtn = page.getByRole('button', { name: 'Book Session' });
+    this.confirmBtn = page.getByRole('button', { name: 'Confirm', exact: true });
+    this.continueCoachBtn = page.getByRole('button', { name: 'Continue to Coach Selection' });
+
+    this.notificationSuccess = page.getByRole('heading', { name: 'Booking Successful! 🎉' });
+  }
+
+  /**
+   * @param isOnline Boolean
+   * @default isOnline true
+   * @desc if you want book online fill true otherwise false
+   */
+  async bookMode(isOnline: boolean = true) {
+    if (isOnline) {
+      await this.bookSessionBtn.click();
+      this.mode[0].click();
+    } else {
+      await this.bookSessionBtn.click();
+      this.mode[1].click();
+    }
+  }
+
+  async bookByCoach(form: CoachForm) {
+    await this.page.getByText(Method.COACH).click();
+    await this.selectCoachBtn.first().click();
+    await this.selectDate(form.date);
+    await this.selectTime(form.time);
+    await this.continueCoachBtn.click();
+    await this.fillFormCoach(form);
+    await this.confirmAndBookBtn.click();
+    await this.confirmBtn.click();
+  }
+
+  async fillFormCoach(form?: CoachForm) {
+    await this.planning.fill(form ? form.planning : '');
+    await this.description.focus();
+    await this.description.pressSequentially(form ? form.description : '', { delay: 50 });
+    await this.friendNamed.fill(form ? form.friendNamed : '');
+  }
+
+  async selectDate(date: string) {
+    return this.page.getByRole('gridcell', { name: date }).click();
+  }
+
+  async selectTime(time: string) {
+    const slotWaktu = this.page.getByRole('button', { name: time });
+    await expect(slotWaktu).toBeVisible({ timeout: 25000 });
+    await slotWaktu.click();
+  }
+}
--- tests/mentee/bookingsession.spec.ts
+++ tests/mentee/bookingsession.spec.ts
@@ -1,36 +1,25 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/shared/login.page';
+import { BookingSession, CoachForm } from '../../pages/mentee/bookingsession';
 
-test('test', async ({ page }) => {
-  await page.goto('https://globy.wins.web.id/login');
-  await page.getByRole('textbox', { name: 'Email Address' }).click();
-  await page.getByRole('textbox', { name: 'Email Address' }).fill('mentee_1776344125520@mail.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('Password123_');
-  await page.getByLabel('Sign In').getByRole('button', { name: 'Sign In' }).click();
-  await page.getByRole('button', { name: 'Book Session' }).click();
-  await page.getByText('Online SessionVideo call').click();
-  await page.getByText('By CoachChoose your preferred').click();
-  await page.getByRole('button', { name: 'Select' }).first().click();
-  await page.getByRole('gridcell', { name: '25' }).click();
-  // Tunggu sampai jam '14:' muncul dan bisa diklik (misal kita beri batas tunggu 15 detik)
-  const slotWaktu = page.getByRole('button', { name: '14:' });
-  await expect(slotWaktu).toBeVisible({ timeout: 15000 });
-  await slotWaktu.click();
-  await page.getByRole('button', { name: 'Continue to Coach Selection' }).click();
-  await page.getByRole('textbox', { name: 'Contoh: Career Planning,' }).click();
-  await page.getByRole('textbox', { name: 'Contoh: Career Planning,' }).fill('Career Planning');
-  await page.getByRole('textbox', { name: 'Add a short description about' }).click();
-  await page.getByRole('textbox', { name: 'Add a short description about' }).fill('For job');
-  await page.getByRole('textbox', { name: 'Masukkan nama teman Anda (' }).click();
-  await page.getByRole('textbox', { name: 'Masukkan nama teman Anda (' }).fill('Salma');
-  await page.getByRole('button', { name: 'Confirm & Book Now' }).click();
-  await page.getByRole('button', { name: 'Confirm & Book Now' }).click();
-  await page.getByRole('button', { name: 'Confirm' }).click();
-  await page.getByRole('button', { name: 'Confirm' }).click();
-  // Memastikan element heading muncul dan teksnya tepat (Booking Successful! 🎉)
-  await expect(page.getByRole('heading', { name: 'Booking Successful! 🎉' })).toBeVisible({
+test('booking Session By Coach', async ({ page }) => {
+  const login = new LoginPage(page);
+  const booking = new BookingSession(page);
+  const form: CoachForm = {
+    planning: 'Career Planning',
+    description: 'For Job Purpos',
+    friendNamed: 'Fahmi',
+    time: '12:',
+    date: '25',
+  };
+
+  await login.goto();
+  await login.login('mentee_1776344125520@mail.com', 'Password123_');
+
+  await booking.bookMode(true);
+  await booking.bookByCoach(form);
+
+  await expect(booking.notificationSuccess).toBeVisible({
     timeout: 14000,
   });
-  // Cara alternatif jika ingin lebih ketat dalam pengecekan string/typo
-  await expect(page.getByRole('heading')).toContainText('Booking Successful!');
 });
