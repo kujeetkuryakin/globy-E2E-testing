b6403e992c0354b6af03faaf3299d5d9f4f6793f
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2026-04-20 15:17:36 +0700

    fix: bug in locator notification success

--- tests/mentee/bookingsession.spec.ts
+++ tests/mentee/bookingsession.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/shared/login.page';
 import { BookingSession, CoachForm } from '../../pages/mentee/bookingsession';
 
-test('booking Session By Coach', async ({ page }) => {
+test('Positive Test: Booking Session By Coach', async ({ page }) => {
   const login = new LoginPage(page);
   const booking = new BookingSession(page);
   const form: CoachForm = {
     planning: 'Career Planning',
     description: 'For Job Purpos',
     friendNamed: 'Fahmi',
-    time: '12:',
+    time: '09:',
     date: '25',
   };
 
@@ -19,7 +19,7 @@ test('booking Session By Coach', async ({ page }) => {
   await booking.bookMode(true);
   await booking.bookByCoach(form);
 
-  await expect(booking.notificationSuccess).toBeVisible({
+  await expect(booking.notificationSuccess.first()).toBeVisible({
     timeout: 14000,
   });
 });
