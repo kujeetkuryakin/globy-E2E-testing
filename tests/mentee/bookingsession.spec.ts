9acc2b17a4db5bbe884a2fc582a097e7162f54ea
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2026-04-22 12:27:27 +0700

    test: update booking session

--- tests/mentee/bookingsession.spec.ts
+++ tests/mentee/bookingsession.spec.ts
@@ -3,23 +3,25 @@ import { LoginPage } from '../../pages/shared/login.page';
 import { BookingSession, CoachForm } from '../../pages/mentee/bookingsession';
 
 test('Positive Test: Booking Session By Coach', async ({ page }) => {
+  test.setTimeout(40000);
   const login = new LoginPage(page);
   const booking = new BookingSession(page);
   const form: CoachForm = {
     planning: 'Career Planning',
     description: 'For Job Purpos',
     friendNamed: 'Fahmi',
-    time: '15:',
-    date: '30',
+    time: '15:00',
+    date: 30,
   };
 
   await login.goto();
-  await login.login('mentee_1776344125520@mail.com', 'Password123_');
+  await login.login('mentee_1776666826136@mail.com', 'Password123_');
 
   await booking.bookMode(true);
+
   await booking.bookByCoach(form);
 
   await expect(booking.notificationSuccess.first()).toBeVisible({
-    timeout: 14000,
+    timeout: 5000,
   });
 });
