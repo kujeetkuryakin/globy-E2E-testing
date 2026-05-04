2c1bd0843f0a1e55ac786e393497f7722976f7e8
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2026-05-04 15:49:01 +0700

    test: booking offline by coach

--- tests/mentee/bookingofflinebycoach.spec.ts
+++ tests/mentee/bookingofflinebycoach.spec.ts
@@ -1,16 +1,15 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../../pages/shared/login.page';
-import { BookingOfflineSession, OfflineTopicForm } from '../../pages/mentee/bookingofflinesession';
+import { BookingOfflineSession, OfflineCoachForm } from '../../pages/mentee/bookingofflinesession';
 
-test('Booking Offline Session By Topic', async ({ page }) => {
+test('Booking Offline Session By Coach', async ({ page }) => {
   test.setTimeout(120000);
 
   const login = new LoginPage(page);
   const booking = new BookingOfflineSession(page);
 
-  const form: OfflineTopicForm = {
+  const form: OfflineCoachForm = {
     office: 'Tebet OfficeTebet, Jakarta',
-    topic: 'Career Planning', // atau sesuaikan dengan topik riil di UI
     planning: 'Career Planning',
     description: 'For Job Purpose',
     friendNamed: 'Friend Name',
@@ -23,5 +22,5 @@ test('Booking Offline Session By Topic', async ({ page }) => {
 
   await booking.bookMode();
   await booking.selectOffice(form.office);
-  await booking.bookByTopic(form);
+  await booking.bookByCoach(form);
 });
