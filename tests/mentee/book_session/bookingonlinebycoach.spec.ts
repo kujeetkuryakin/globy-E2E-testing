efcfdfc44811f4851009c977b7ec3f50e7a3c054
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2026-05-11 10:31:40 +0700

    fix: refactoring fitur book session

--- tests/mentee/book_session/bookingonlinebycoach.spec.ts
+++ tests/mentee/book_session/bookingonlinebycoach.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/shared/login.page';
-import { BookingOnlineSession, CoachForm } from '../../pages/mentee/bookingonlinesession';
+import { LoginPage } from '../../../pages/shared/login.page';
+import { BookingOnlineSession, CoachForm } from '../../../pages/mentee/bookingonlinesession';
 
 test('Positive Test: Booking Session Online By Coach', async ({ page }) => {
   test.setTimeout(40000);
@@ -15,7 +15,7 @@ test('Positive Test: Booking Session Online By Coach', async ({ page }) => {
   };
 
   await login.goto();
-  await login.login('mentee_1776666826136@mail.com', 'Asaa@3579');
+  await login.login('ahmadzyddannashevy@gmail.com', 'Password123_');
   await page.goto('/dashboard');
   await booking.bookMode(true);
   await booking.bookByCoach(form);
--- tests/mentee/book_session/bookingonlinebytime.spec.ts
+++ tests/mentee/book_session/bookingonlinebytime.spec.ts
@@ -1,25 +1,24 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/shared/login.page';
-import { BookingOnlineSession, TopicForm } from '../../pages/mentee/bookingonlinesession';
+import { LoginPage } from '../../../pages/shared/login.page';
+import { BookingOnlineSession, CoachForm } from '../../../pages/mentee/bookingonlinesession';
 
-test('Positive test: Booking Session Online By Topic', async ({ page }) => {
+test('Positive test: Booking Session Online By Date & Time', async ({ page }) => {
   test.setTimeout(60000);
   const login = new LoginPage(page);
   const booking = new BookingOnlineSession(page);
-  const form: TopicForm = {
+  const form: CoachForm = {
     planning: 'Career Planning',
     description: 'For Job Purpos',
     friendNamed: 'Fahmi',
     time: '15:00',
-    date: 22,
-    topic: 'Public Speaking',
+    date: 29,
   };
 
   await login.goto();
-  await login.login('mentee_1776666826136@mail.com', 'Asaa@3579');
+  await login.login('ahmadzyddannashevy@gmail.com', 'Password123_');
 
   await booking.bookMode(true);
-  await booking.bookByTopic(form);
+  await booking.bookByDateTime(form);
 
   await expect(booking.notificationSuccess.first()).toBeVisible({ timeout: 5000 });
 });
